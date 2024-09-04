08dc912399ecf2efe7e9339e151f946d8341f0a4
YonugJones <peterckerfoot@gmail.com>
2024-09-04 11:27:09 -0500

    Created server

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 import { createServer } from 'node:http';
 
+const hostname = 'localhost';
+const port = 8080;
+
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end();
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
+});
